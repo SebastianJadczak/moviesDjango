2c68528ce8cb2f761ba4585f67c8adaf77e04b7f
sjadczak <sebastian-jadczak@wp.pl>
2020-01-14 14:10:08 +0100

    add relations with database

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Review, Aktor
+
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
@@ -16,3 +17,6 @@ class MovieAdmin(admin.ModelAdmin):
     # Wyszukiwanie po kategoriach
     search_fields = ('name', 'description', 'year', 'relased', 'imdb_rating')
 
+#admin.site.register(ExtraInfo)
+admin.site.register(Review)
+admin.site.register(Aktor)
--- main/models.py
+++ main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Movie(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(default="")
@@ -10,8 +9,31 @@ class Movie(models.Model):
     imdb_rating = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=7)
     photo = models.ImageField(null=True, blank=True, upload_to='Filmy')
 
+
     def __str__(self):
         return self.name_width_year()
 
     def name_width_year(self):
         return str(self.name) + " (" + str(self.year) + ")"
+
+
+class Review(models.Model):
+    text = models.CharField(default='', blank=True, max_length=120)
+    stars = models.IntegerField(default=5)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+class Aktor(models.Model):
+    imie = models.CharField(max_length=128)
+    nazwisko = models.CharField(max_length=120)
+    filmy = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.show_name_actor()
+
+    def show_name_actor(self):
+        return str(self.imie) + " " + str(self.nazwisko)
+
+
